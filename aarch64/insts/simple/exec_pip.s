122591fdcca5afc74ad92d846c176583d6d7d094
hao.chen <hao.chen@cixtech.com>
2022-11-07 11:24:20 +0800

    adr,adrp test

--- aarch64/insts/simple/exec_pip.s
+++ aarch64/insts/simple/exec_pip.s
@@ -16,10 +16,30 @@ bootcode:
 
                 # add imm
                 add x10, x0, #4
-                # add imm sh=0
                 add x10, x0, #4, LSL #0
-                # add imm sh=1
                 add x10, x0, #4, LSL #12
+
+                # adds
+                adds x10, x0, #4
+                adds x10, x5, #4, LSL #0
+                adds x10, x3, #4, LSL #12
+
+                # sub imm
+                sub x10, x0, #4
+                sub x10, x0, #4, LSL #0
+                sub x10, x0, #4, LSL #12
+
+                # subs
+                subs x10, x0, #4
+                subs x10, x5, #4, LSL #0
+                subs x10, x3, #4, LSL #12
+
+                # and, orr, eor, ands
+                and x10, x1, #0x7ff
+                orr x10, x1, #0x7ff
+                eor x10, x1, #0x7ff
+                ands x10, x1, #0x7ff
+                and	w10, w1, #0xc1ffc1ff
 
                 # add shift register, lat=1, pip=I
                 add x10, x0, x1, LSL #0
@@ -43,6 +63,9 @@ bootcode:
 
                 add w11, w1, w6, SXTB #2
                 add x11, x1, w6, SXTB #2
+
+                adr x12, data
+                adrp x13, data
 
 
                 # exit simulator
